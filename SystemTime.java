3a3d4a612f5e2a04c3b0cd48c5f4907f7db70e7a
HirahataArthur <hirahataarthur@gmail.com>
2026-09-03 13:32:24 -0300

    gitignore + joining System date time exercise

---
+++ SystemTime.java
@@ -0,0 +1,7 @@
+import java.util.Date;  
+public class SystemTime {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.print("Current date and time:\n" + date);
+    }
+}
