4517cb21b332bf87b6e36d65f75e4b53420bfc77
HirahataArthur <hirahataarthur@gmail.com>
2026-09-18 19:55:15 -0300

    Do While

--- exercises/repetitionstructures/Main.java
+++ exercises/repetitionstructures/Main.java
@@ -1,17 +1,17 @@
 package repetitionstructures;
-
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
 
-        //USING WHILE
+        // Using a while loop
         int nn = 0;
         while (nn<=4){
             System.out.println("nn is equal to " + nn);
             nn++;
         }
 
-        //WHILE WITH CONDITIONALS
+        // While loop with conditionals
         int nn2 = 0;
         while(nn2<=10){
             if (nn2 == 0){
@@ -23,7 +23,7 @@ public class Main{
             }
             nn2++;
         }
-        //INTERRUPTING WHILE
+        // Interrupting a while loop
         int nn3 = 10;
         while (nn3>0){
             if (nn3 == 4){
@@ -32,5 +32,37 @@ public class Main{
             System.out.println("NN3 is equal to: " + nn3);
             nn3--;
         }
+
+        // Do-while loop
+        int nn4 = 0;
+        do{                         // Executes the code inside before checking the condition
+            System.out.print(nn4 + "\n");
+            nn4++;
+        } while (nn4<=4);
+
+
+        int nn5 = 7;
+        do {
+            System.out.println("This is an even number"); // Prints even though the condition returns false
+        } while((7%2)==0); // Only possible because the computer runs the code before checking the condition
+
+        //Example-1
+        int n,s =0;
+        String result;
+        Scanner input = new Scanner(System.in);
+        do {
+            try {
+                System.out.print("Type a number: ");
+                n = input.nextInt();
+                s += n;
+            } catch (Exception e) {
+                System.out.println("Please insert a valid number.\n");
+                input.next();
+            }
+
+            System.out.print("Continue? [Y/N]\n");
+            result = input.next().toUpperCase();
+        } while (result.equals("Y"));
+        System.out.print("Summing up all the entered values: " + s);
     }
 }
